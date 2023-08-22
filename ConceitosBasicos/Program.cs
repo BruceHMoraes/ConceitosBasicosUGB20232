91591fbc81616a5f834373228e6cb5d0d592d01e
2022101251 <2022101251@LAB2-015>
2023-08-22 20:15:12 -0300

    Adicionar arquivos de projeto.

---
+++ ConceitosBasicos/Program.cs
@@ -0,0 +1,17 @@
+﻿using ConceitosBasicos;
+
+int num1 = 20;
+int num2 = 11, result;
+
+Somador Soma; 
+Soma = new Somador();
+
+result = Soma.Soma(num1, num2);
+
+Console.WriteLine($" A soma dos numeros {num1} + {num2} é {result}" );
+
+//Conversor MetrosMilimetros;
+//MetrosMilimetros = new Conversor();
+Console.WriteLine(Conversor.MetrosMilimetros(1.76));
+double metros = 1.76;
+Console.WriteLine("A medida {0} metros correspondente a {1}", metros, Conversor.MetrosMilimetros(1.76));
